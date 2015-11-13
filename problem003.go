f54776678b631d3b0a5e6e023f96ff9a1bba2ee9
Kyle Chadha <kyle.chadha@gmail.com>
2015-11-13 08:55:38 -0500

    Comment

--- problem003.go
+++ problem003.go
@@ -6,12 +6,18 @@ import (
 	"time"
 )
 
+// Problem 3
+// The prime factors of 13195 are 5, 7, 13 and 29.
+// What is the largest prime factor of the number 600851475143 ?
+
 func main() {
+	// Let's do a bit of back-of-the-envelope profiling.
 	start := time.Now()
 
 	f := primeFactors(600851475143)
 	fmt.Println(f)
 
+	// Solves in 200µs! A more traditional approach (better suited to a larger n) solves at 5-7ms.
 	elapsed := time.Since(start)
 	log.Printf("Factorization took %s", elapsed)
 }
